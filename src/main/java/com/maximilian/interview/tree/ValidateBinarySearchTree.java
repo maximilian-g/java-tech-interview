387cc2249e56703c29acf7bf8a70ed050067c2be
n0n4m3q <hearthstone.maximilian@gmail.com>
2024-06-02 11:34:53 +0300

    ValidateBinarySearchTree corrected

--- src/main/java/com/maximilian/interview/tree/ValidateBinarySearchTree.java
+++ src/main/java/com/maximilian/interview/tree/ValidateBinarySearchTree.java
@@ -1,5 +1,8 @@
 package com.maximilian.interview.tree;
 
+import java.util.Stack;
+import java.util.function.Predicate;
+
 /**
  * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
  * <p>
@@ -13,20 +16,51 @@ package com.maximilian.interview.tree;
  */
 public class ValidateBinarySearchTree {
 
+
+    public static boolean isValidBSTStack(TreeNode<Integer> root) {
+        if (root == null) {
+            return true;
+        }
+        Stack<TreeNode<Integer>> stack = new Stack<>();
+        TreeNode<Integer> pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (pre != null && root.val <= pre.val) {
+                return false;
+            }
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
+
     public static boolean isValidBST(TreeNode<Integer> root) {
+        return isValidBSTInternal(root, null);
+    }
+
+    public static boolean isValidBSTInternal(TreeNode<Integer> root, Predicate<Integer> compliesWithParent) {
+        if (root == null) {
+            return true;
+        }
         // all that is left from root must be < root.val,
         // all that is right from root must be > root.val
         if (root.left != null) {
-            if (root.val <= root.left.val || !isValidBST(root.left)) {
+            if (root.val <= root.left.val ||
+                    !isValidBSTInternal(root.left, (val -> root.val > val && (compliesWithParent == null || compliesWithParent.test(val))))) {
                 return false;
             }
         }
         if (root.right != null) {
-            if (root.val >= root.right.val || !isValidBST(root.right)) {
+            if (root.val >= root.right.val ||
+                    !isValidBSTInternal(root.right, (val -> root.val < val && (compliesWithParent == null || compliesWithParent.test(val))))) {
                 return false;
             }
         }
-        return true;
+        return compliesWithParent == null || compliesWithParent.test(root.val);
     }
 
 }
--- src/test/java/com/maximilian/interview/tree/ValidateBinarySearchTreeTest.java
+++ src/test/java/com/maximilian/interview/tree/ValidateBinarySearchTreeTest.java
@@ -7,15 +7,20 @@ class ValidateBinarySearchTreeTest {
 
     @Test
     void isValidBST() {
-        TreeNode<Integer> root = new TreeNode<>();
-        root.val = 2;
-        root.left = new TreeNode<>();
-        root.left.val = 1;
-        root.right = new TreeNode<>();
-        root.right.val = 3;
+        TreeNode<Integer> root = new TreeNode<>(2);
+        root.left = new TreeNode<>(1);
+        root.right = new TreeNode<>(3);
 
+        Assertions.assertTrue(ValidateBinarySearchTree.isValidBSTStack(root));
         Assertions.assertTrue(ValidateBinarySearchTree.isValidBST(root));
         root.right.val = -5;
+        Assertions.assertFalse(ValidateBinarySearchTree.isValidBSTStack(root));
+        Assertions.assertFalse(ValidateBinarySearchTree.isValidBST(root));
+
+        root = new TreeNode<>(5);
+        root.left = new TreeNode<>(4);
+        root.right = new TreeNode<>(6, new TreeNode<>(3), new TreeNode<>(7));
+        Assertions.assertFalse(ValidateBinarySearchTree.isValidBSTStack(root));
         Assertions.assertFalse(ValidateBinarySearchTree.isValidBST(root));
 
     }
